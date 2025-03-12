578ef9522defa69eeab977eff5d712e2aa450d85
Shyshkin Andrey <shyshkinandrey06@gmail.com>
2025-03-12 11:53:09 +0200

    Bug fix

--- src/app/Logic/Game.js
+++ src/app/Logic/Game.js
@@ -1,26 +1,26 @@
-import { Time } from "./Time"
-import { Input } from "./Input"
+import { Input } from './Input'
+import { Time } from './Time'
 export class Game {
-  ShouldStop = false
-  IsPause = false
-  constructor(onStart, onUpdate, onStop, onStartPause, onClearPause) {
-    this.onStart = onStart
-    this.onUpdate = onUpdate
-    this.onStop = onStop
-    this.onStartPause = onStartPause
-    this.onClearPause = onClearPause
-  }
-  Start() {
-    this.onStart()
-    Time.Init()
-    Input.Init()
-    window.requestAnimationFrame(this.Update.bind(this))
-  }
-  Update() {
-    if (!this.ShouldStop) {
-      Time.Update()
-      this.onUpdate()
-      window.requestAnimationFrame(this.Update.bind(this))
-    }
-  }
+	ShouldStop = false
+	IsPause = false
+	constructor(onStart, onUpdate, onStop, onStartPause, onClearPause) {
+		this.onStart = onStart
+		this.onUpdate = onUpdate
+		this.onStop = onStop
+		this.onStartPause = onStartPause
+		this.onClearPause = onClearPause
+	}
+	Start() {
+		this.onStart()
+		Time.Init()
+		Input.Init()
+		window.requestAnimationFrame(this.Update.bind(this))
+	}
+	Update() {
+		if (!this.ShouldStop) {
+			Time.Update()
+			this.onUpdate()
+			window.requestAnimationFrame(this.Update.bind(this))
+		}
+	}
 }
